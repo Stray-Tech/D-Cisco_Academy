6879e5c358c7208b284d16c0d30699dba3dc5910
Saif Uddin <admin@saifuddin.uk>
2024-07-11 21:08:43 +0100

    burger button working in blog article page

--- assets/js/nav-buttons.js
+++ assets/js/nav-buttons.js
@@ -1,16 +1,44 @@
-const burgerBtnElements = document.querySelectorAll('#burger-button, #burger-img');
+// const burgerBtnElements = document.querySelectorAll('#burger-button, #burger-img');
 
-for(let i=0; i<burgerBtnElements.length; i++){
-    burgerBtnElements[i].addEventListener('click', ()=>{
-        const categoriesNav = document.querySelector('.dropdown');    
-        categoriesNav.style.display = 'inline-block';
-    })
+// for(let i=0; i<burgerBtnElements.length; i++){
+//     burgerBtnElements[i].addEventListener('click', ()=>{
+//         const categoriesNav = document.querySelector('.dropdown');    
+//         categoriesNav.style.display = 'inline-block';
+//     })
+// }
+
+// const categoriesContent = document.querySelector('.dropdown-content');    
+// categoriesContent.addEventListener('click', ()=>{
+//     categoriesContent.style.display = 'inline-block';
+// })
+
+function burgerMenu(event){
+    console.log(event)
+    const categoriesNav = document.querySelector('#top-nav');
+    const burgerIcon = document.querySelector('#burger-img');
+
+    categoriesNav.classList.toggle('reveal');
+    burgerIcon.classList.toggle('burger-close-icon');
+    burgerIcon.classList.toggle('burger-icon');
+
+    if ('burger-icon' == burgerIcon.className){
+        burgerIcon.src = './assets/images/burger.png'
+        burgerIcon.alt = 'burger icon'
+    }
+    else if ('burger-close-icon' == burgerIcon.className){
+        burgerIcon.src = './assets/images/icons8-close-window-50.png'
+        burgerIcon.alt = 'burger close icon'
+    }
 }
 
-const categoriesContent = document.querySelector('.dropdown-content');    
-categoriesContent.addEventListener('click', ()=>{
-    categoriesContent.style.display = 'inline-block';
-})
+// function 
 
+addGlobalEventListener('click', burgerMenu, '#burger-img');
+addGlobalEventListener('click', burgerMenu, '#burger-button');
 
-console.log(burgerBtn)
+function addGlobalEventListener(typeOfEvent, callback, selector, stopPropagation=true) {
+    document.addEventListener(typeOfEvent, (eventObj) => {
+        if (eventObj.target.matches(selector)) callback(eventObj);
+        if (stopPropagation) eventObj.stopPropagation();
+    })
+}
