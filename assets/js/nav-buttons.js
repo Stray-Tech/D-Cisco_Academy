5ecb1b3de33d3942824112bacae22bd489de152a
Saif Uddin <admin@saifuddin.uk>
2024-07-12 09:34:48 +0100

    added top-nav id to all pages

--- assets/js/nav-buttons.js
+++ assets/js/nav-buttons.js
@@ -1,17 +1,3 @@
-// const burgerBtnElements = document.querySelectorAll('#burger-button, #burger-img');
-
-// for(let i=0; i<burgerBtnElements.length; i++){
-//     burgerBtnElements[i].addEventListener('click', ()=>{
-//         const categoriesNav = document.querySelector('.dropdown');    
-//         categoriesNav.style.display = 'inline-block';
-//     })
-// }
-
-// const categoriesContent = document.querySelector('.dropdown-content');    
-// categoriesContent.addEventListener('click', ()=>{
-//     categoriesContent.style.display = 'inline-block';
-// })
-
 function burgerMenu(event){
     console.log(event)
     const categoriesNav = document.querySelector('#top-nav');
