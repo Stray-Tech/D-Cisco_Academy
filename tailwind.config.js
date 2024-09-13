8fc87ec9d3524c4255a262d080fad2e6aa0a070a
Saif Uddin <admin@saifuddin.uk>
2024-09-13 11:02:08 +0100

    fixed font imports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}', './public/**/*.html'],
   theme: {
     extend: {
       fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-        lexend: ['Lexend', 'sans-serif'],
-        mulish: ['Mulish', 'sans-serif'],
+        poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
+        lexend: ['Lexend', ...defaultTheme.fontFamily.sans],
+        mulish: ['Mulish', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         customBgColor: '#FFFAFE',
